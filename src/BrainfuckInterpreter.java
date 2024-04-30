525632a34e79230159b8c3af34366c24e3b8080a
Francesco Marastoni <49027005+Francesco146@users.noreply.github.com>
2024-04-30 23:55:21 +0200

    add brainfuck interpreter

---
+++ src/BrainfuckInterpreter.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class BrainfuckInterpreter extends BrainfuckBaseVisitor<Byte> {
+    private final int LENGTH = 65443;
+    private final byte[] tape = new byte[LENGTH];
+    private int dataPointer = 0;
+    private static Scanner IN = new Scanner(System.in);
+
+    @Override
+    public Byte visitMain(BrainfuckParser.MainContext ctx) {
+        return visit(ctx.com());
+    }
+
+
+    @Override
+    public Byte visitLoop(BrainfuckParser.LoopContext ctx) {
+
+        while (tape[dataPointer] != 0) visit(ctx.com(0));
+
+        return visit(ctx.com(1));
+    }
+
+    @Override
+    public Byte visitLt(BrainfuckParser.LtContext ctx) {
+        dataPointer = (dataPointer == 0) ? LENGTH - 1 : dataPointer - 1;
+        return visit(ctx.com());
+    }
+
+    @Override
+    public Byte visitGt(BrainfuckParser.GtContext ctx) {
+        dataPointer = (dataPointer == LENGTH - 1) ? 0 : dataPointer + 1;
+        return visit(ctx.com());
+    }
+
+    @Override
+    public Byte visitPlus(BrainfuckParser.PlusContext ctx) {
+        tape[dataPointer]++;
+        return visit(ctx.com());
+    }
+
+    @Override
+    public Byte visitMinus(BrainfuckParser.MinusContext ctx) {
+        tape[dataPointer]--;
+        return visit(ctx.com());
+    }
+
+    @Override
+    public Byte visitDot(BrainfuckParser.DotContext ctx) {
+        System.out.print((char) tape[dataPointer]);
+        return visit(ctx.com());
+    }
+
+    @Override
+    public Byte visitComma(BrainfuckParser.CommaContext ctx) {
+        try {
+            do {
+                tape[dataPointer] = (byte) System.in.read();
+            } while (tape[dataPointer] == '\n');
+        }
+        catch (IOException e) { throw new RuntimeException(e); }
+
+        return visit(ctx.com());
+    }
+
+    @Override
+    public Byte visitNil(BrainfuckParser.NilContext ctx) {
+        return '\0';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        InputStream is = cl.getResourceAsStream(args[0]);
+        assert is != null;
+        CharStream cs = CharStreams.fromStream(is);
+
+        BrainfuckLexer lexer = new BrainfuckLexer(cs);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        BrainfuckParser parser = new BrainfuckParser(tokens);
+
+        ParseTree tree = parser.main();
+
+        BrainfuckInterpreter interpreter = new BrainfuckInterpreter();
+        interpreter.visit(tree);
+    }
+}
